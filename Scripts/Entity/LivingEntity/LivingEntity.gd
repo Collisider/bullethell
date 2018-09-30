75bf29f6da4e8fd0da36a5b60d4cdaea98a7c717
RealMelwei <melvin@weissprivat.de>
2018-09-30 20:50:11 +0200

    Abstraktes Klassenzeugs...

---
+++ Scripts/Entity/LivingEntity/LivingEntity.gd
@@ -0,0 +1,12 @@
+extends "res://Scripts/Entity/Entity.gd"
+
+export var hitpoints = 1
+
+func take_damage(damage, attacker = null):
+	print("ow!")
+	hitpoints -= damage
+	if hitpoints <= 0:
+		die()
+
+func die():
+	pass
