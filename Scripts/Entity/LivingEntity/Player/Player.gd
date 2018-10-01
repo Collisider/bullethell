fb8855097eb4e38612fdcf8d9db28be3e5936470
RealMelwei <melvin@weissprivat.de>
2018-10-01 18:44:58 +0200

    Player stats & more

--- Scripts/Entity/LivingEntity/Player/Player.gd
+++ Scripts/Entity/LivingEntity/Player/Player.gd
@@ -1,15 +1,52 @@
-extends KinematicBody2D
+extends "res://Scripts/Entity/LivingEntity/LivingEntity.gd"
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+const INVINCIBILITY_DURATION = 0.6
+
+var speed = 1
+var fire_rate = 1
+var base_damage = 50
+var base_bullet_speed = 5
+var base_range = 5
+var max_health = 3
+
+var shooting = false
+
+var invincibility = 0
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _input(event):
+	if event is InputEventKey or event is InputEventMouseButton:
+		if Input.is_action_pressed("move_right"):
+			key_force.x += 1
+		if Input.is_action_pressed("move_left"):
+			key_force.x -= 1
+		if Input.is_action_pressed("move_down"):
+			key_force.y += 1
+		if Input.is_action_pressed("move_up"):
+			key_force.y -= 1
+		if Input.is_action_pressed("shoot"):
+			shooting = true
+		if Input.is_action_just_released("shoot"):
+			shooting = false
+
+func _process(delta):
+	
+	pass
+
+func take_damage(damage, attacker = null):
+	if invincibility <= 0:
+		.take_damage(damage)
+		
+		invincibility = INVINCIBILITY_DURATION
+
+func heal(amount):
+	if hitpoints + amount <= max_health:
+		hitpoints += amount
+	else:
+		hitpoints = max_health
+
+
